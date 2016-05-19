724693bf262a62df3a91b9dea9612d6d8ef74424
evandrocoan <evandrocoan@hotmail.com>
2016-05-19 14:21:22 -0300

    Migrate the documents binary files to drive folder and placed an public link to it.

--- EPOS_MotesII_Lamps/compileApplication.sh
+++ EPOS_MotesII_Lamps/compileApplication.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+#
+# Saves the current opened path, to restore it when this scripts finish.
+#
 PWD_COMPILE_EPOS_LAMP=$(pwd)
+
+#
+# 
+#
+PROGRAM_TO_COMPILE=
 cp $EPOS/app/INE5412_EposMotesII_SmartObjects/EPOS_MotesII_Lamps/structuredLEDControl.cc $EPOS/app
 cd $EPOS ; make veryclean all; make APPLICATION=structuredLEDControl ;  arm-objcopy -I elf32-little -O binary img/structuredLEDControl.img img/structuredLEDControl.bin
 rm $EPOS/app/structuredLEDControl.cc
