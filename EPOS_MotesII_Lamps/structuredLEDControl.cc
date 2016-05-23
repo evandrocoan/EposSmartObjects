1c577f04c8c469c523c955585e644ff381ac50cb
evandrocoan <evandrocoan@hotmail.com>
2016-05-23 12:47:49 -0300

    Created a header to save the Lucas' array operations.

--- EPOS_MotesII_Lamps/structuredLEDControl.cc
+++ EPOS_MotesII_Lamps/structuredLEDControl.cc
@@ -49,6 +49,9 @@
 #include <semaphore.h>
 #include <traits.h>
 
+#include <array_operations.h>
+
+
 __USING_SYS
 
 //TODO
@@ -69,74 +72,7 @@ __USING_SYS
  }
  */
 
-/**
- * Change this typedef to change the type of the element for the bit vector.
- * Rule is for it to be small enough to waste less memory;
- */
-typedef unsigned char bitarr_el;
-
-/**
- * Abstraction provided to access a bitarray. Caution: do not access it with []
- */
-typedef bitarr_el* bitarr_t;
-
-/**
- * Works like a C++ constructor.
- * @param arrSize		The size of the array
- * @return				A pointer to the array.
- */
-bitarr_t createBitArray(size_t arrSize) {
-	bitarr_t ar;
-	ar = new bitarr_el[arrSize/sizeof(bitarr_el) + 1];
-	return ar;
-}
-
-/**
- * Frees memory from bit array;
- * @param arr			Memory to be freed
- */
-void destroyBitArray(bitarr_t arr) {
-	delete[] arr;
-}
-
-/**
- * Checks to see if the bit at the given position is set
- * @param arr			The bit array
- * @param pos			The position
- * @return				true if bit is 1 else false
- */
-bool checkPosition(bitarr_t arr, size_t pos) {
-	//Calculate region
-	size_t region = pos/sizeof(bitarr_el);
-	//Get element at region
-	bitarr_el* element = arr + region;
-	//Calculate index inside element
-	size_t elementPos = pos % sizeof(bitarr_el);
-	//Check bit of element
-	return (*element) & (1 << (elementPos));
-}
-/**
- * Sets bit on given position of the array
- * @param arr			The bit array
- * @param pos			The position in the array
- * @param value			If value is true, sets bit to 1 else 0.
- */
-void setPosition(bitarr_t arr, size_t pos, bool value) {
-	//Calculate region
-	size_t region = pos/sizeof(bitarr_el);
-	//Get element at region
-	bitarr_el* element = arr + region;
-	//Calculate index inside element
-	size_t elementPos = pos % sizeof(bitarr_el);
-	if(value) {
-		*element |= (1 << elementPos);
-	}
-	else {
-		*element &= !(1 << elementPos);
-	}
-}
-
-
+ 
 bool useSensor = false;
 
 int defaultPower( int power )
