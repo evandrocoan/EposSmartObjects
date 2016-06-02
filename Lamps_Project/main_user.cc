d9c4e90ed320a5dda94254a5a5f4a6c854a33d40
evandrocoan <evandrocoan@hotmail.com>
2016-06-02 18:27:08 -0300

    Created the main user and lamp board application initializers.

---
+++ Lamps_Project/main_user.cc
@@ -0,0 +1,66 @@
+/**
+ * Structured LED EposMotesII Controlling
+ *
+ * This work is licensed under the EPOS Software License v1.0.
+ * A copy of this license is available at the EPOS system source tree root.
+ * Note that EPOS Software License applies to both source code and executables.
+ *
+ *  You should have received a copy of the EPOS Software License v1.0 along
+ *  with this program. If not, see
+ *  <http://epos.lisha.ufsc.br/EPOS+Software+License+v1.0>.
+ *
+ * *****************************************************************************
+ * 
+ * 
+*/
+
+#include <utility/ostream.h>
+#include <machine.h>
+#include <alarm.h>
+#include <sensor.h>
+#include <battery.h>
+#include <uart.h>
+#include <thread.h>
+#include <mutex.h>
+#include <semaphore.h>
+#include <traits.h>
+#include <headers/lamps_project_debugger.h>
+#include <headers/array_operations.h>
+#include <classes/MyClass.h>
+
+
+
+/**
+ * #define __SYS_NS	   System
+ * #define __USING_SYS using namespace __SYS_NS
+ */
+__USING_SYS;
+
+
+
+/**
+ * Main function entry point. To creates and configures the user smart object application.
+ */
+int main()
+{
+    const char* const PROGRAM_VERSION = "0.1";
+    
+    PRINTLN( 1, "EposMotesII app MAIN_USER initing..." );
+    PRINTLN( 1, "Program version: " << PROGRAM_VERSION );
+    
+    
+    
+    PRINTLN( 1, "EposMotesII app finishing" );
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
